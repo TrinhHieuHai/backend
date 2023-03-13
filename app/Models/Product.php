12101634db7999a6c691d6e395131f3cbdfaf6d6
trinhhieuhai <fantasy952001@gmail.com>
2023-03-13 23:19:44 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $table='products';
+    protected $fillable =[
+        'category_id',
+        'meta_title',
+        'meta_keyword',
+        'meta_descrip',
+        'slug',
+        'name',
+        'description',
+        'brand',
+        'selling_price',
+        'original_price',
+        'qty',
+        'image',
+        'featured',
+        'popular',
+        'status',
+    ];
+
+    protected $with=['category'];
+    public function category()
+    {
+        return $this->belongsTo(category::class,'category_id','id');
+    }
+}
